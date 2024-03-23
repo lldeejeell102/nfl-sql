aa70cc4288654cc01da65872e61e067457b84355
Daniel Ji <daniel.ji0923@gmail.com>
2024-03-23 13:26:39 -0700

    NFL queries 1-6

--- nfl.sql
+++ nfl.sql
@@ -1,21 +1,48 @@
 -- 1. List the names of all NFL teams'
 
-
+-- List all teams in ascending order
+-- SELECT name FROM
+--   teams
+-- ORDER BY teams.name ASC
 
 -- 2. List the stadium name and head coach of all NFC teams
 
+-- List all stadium and head coach names from teams table where the conference is NFC. Have all statdium names in ascending order.
+-- SELECT Stadium, head_coach FROM
+--   teams
+-- WHERE conference = 'NFC'
+-- ORDER BY teams.Stadium ASC
 
 -- 3. List the head coaches of the AFC South
 
+-- List all head coach names from teams table where the conference is NFC and division is South. Have all head coach names in ascending order.
+-- SELECT head_coach FROM
+--   teams
+-- WHERE conference = 'NFC'
+-- AND division = 'South'
+-- ORDER BY teams.head_coach ASC
 
 -- 4. The total number of players in the NFL
 
+-- Count all ids from players table
+-- SELECT count(id) FROM
+--   players
 
 -- 5. The team names and head coaches of the NFC North and AFC East
 
+-- Show all team name and head coach name of teams in the NFC conference and North division and AFC conference and East division.
+-- SELECT name, head_coach from
+--   teams
+-- WHERE conference = 'NFC' AND division = 'North'
+-- OR conference = 'AFC' AND division = 'East'
 
 -- 6. The 50 players with the highest salaries
 
+-- Show name and salary columns from players table. Show in descending order of salary and limit to 50.
+-- SELECT name, salary from
+--   players
+-- ORDER BY salary DESC
+-- LIMIT 50
 
 -- 7. The average salary of all NFL players
 
